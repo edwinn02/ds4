60bba21e417b2864b9d44b564246c087b8587140
edwinn02 <edwin.moreno@utp.ac.pa>
2025-09-29 23:04:39 -0500

    Finalizado2

---
+++ Laboratorio 8/Laboratorio 8-1/Laboratorio 8-8/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+abstract class ClaseAbstracta
+{
+    // Métodos abstractos: obligan a las clases hijas a implementarlos
+    protected abstract string tomarValor();
+    public abstract string prefixValor(string prefix);
+
+    // Método común para todas las clases hijas
+    public void printOut()
+    {
+        Console.WriteLine(tomarValor());
+    }
+}
+
+class ClaseConcreta1 : ClaseAbstracta
+{
+    protected override string tomarValor()
+    {
+        return "ClaseConcreta1";
+    }
+
+    public override string prefixValor(string prefix)
+    {
+        return $"{prefix}ClaseConcreta1";
+    }
+}
+
+class ClaseConcreta2 : ClaseAbstracta
+{
+    protected override string tomarValor()
+    {
+        return "ClaseConcreta2";
+    }
+
+    public override string prefixValor(string prefix)
+    {
+        return $"{prefix}ClaseConcreta2";
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ClaseConcreta1 concreta1 = new ClaseConcreta1();
+        concreta1.printOut();
+        Console.WriteLine(concreta1.prefixValor("ES_"));
+
+        ClaseConcreta2 concreta2 = new ClaseConcreta2();
+        concreta2.printOut();
+        Console.WriteLine(concreta2.prefixValor("ES_"));
+    }
+}
