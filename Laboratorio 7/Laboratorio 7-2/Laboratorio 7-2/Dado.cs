050bfc49201018679b6e540074cc82129a5926a5
edwinn02 <edwin.moreno@utp.ac.pa>
2025-11-22 14:56:38 -0500

    Añadir laboratorios nuevos

---
+++ Laboratorio 7/Laboratorio 7-2/Laboratorio 7-2/Dado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Dado
+{
+    private int valor;
+    private static Random aleatorio = new Random();
+
+    public Dado()
+    {
+        // No es necesario crear un nuevo Random aquí si ya es static
+    }
+
+    public void Tirar()
+    {
+        valor = aleatorio.Next(1, 7);   // Número entre 1 y 6
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine("El valor del dado es: " + valor);
+    }
+
+    public int RetornarValor()
+    {
+        return valor;
+    }
+}
