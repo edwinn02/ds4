e8d3515f9588d66c6ce5cbf6ed27a2778a1355bf
edwinn02 <edwin.moreno@utp.ac.pa>
2025-11-22 14:56:38 -0500

    Añadir laboratorios nuevos

---
+++ Laboratorio 10/Laboratorio 10-1/Laboratorio 10-1/Program.vb
@@ -0,0 +1,9 @@
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hola")
+        Console.WriteLine("Este es mi primer programa de consola")
+        Console.ReadKey()
+    End Sub
+
+End Module
