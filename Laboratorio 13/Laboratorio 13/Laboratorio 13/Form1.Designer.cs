f67a742741af0ef05bd6f5a6bdf8d995fe139f3f
edwinn02 <edwin.moreno@utp.ac.pa>
2025-11-24 12:40:46 -0500

    Añadir documentos faltantes y actualizaciones

---
+++ Laboratorio 13/Laboratorio 13/Laboratorio 13/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Laboratorio_13
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.ListBox listBox1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(76, 30);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(194, 68);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(20, 143);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 164);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 360);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "Form1";
+            this.Text = "Conexión SQL";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Laboratorio 13/Laboratorio 13/Laboratorio 13/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Laboratorio_13
+{
+    public partial class Form1 : Form
+    {
+        // Cadena de conexión correcta para tu servidor e instancia
+        private string connectionString =
+            @"Server=DESKTOP-SMNNS8P\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(connectionString);
+
+            try
+            {
+                conexion.Open();
+                MessageBox.Show("Conexión abierta.");
+
+                string query = "SELECT ProductName FROM [dbo].[Products]";
+                SqlCommand cmd = new SqlCommand(query, conexion);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                listBox1.Items.Clear();
+
+                while (reader.Read())
+                {
+                    listBox1.Items.Add(reader["ProductName"].ToString());
+                }
+
+                reader.Close();
+                conexion.Close();
+                MessageBox.Show("Conexión cerrada.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
